1395da58dd448ee18431e04a1d45e84d105cb0f7
unknown <zequini.leonardo@gmail.com>
2024-12-06 13:40:51 -0300

    roles and permissions

---
+++ app/Controllers/Http/RoleController.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const Role = use ("Role")
+class RoleController {
+    async index(){
+        return await Role.all()
+    }
+
+    async show({params}){
+        const role = await Role.findOrFail(params.id)
+        await role.load('Permissions')
+        return role
+    }
+
+    async store({request}){
+        const {permissions, ...data} = request.only([
+            "name",
+            "slug",
+            "description",
+            "permissions"
+        ])
+
+        const role = await Role.create(data)
+
+        if(permissions){
+            await role.permissions().attach(permissions)
+        }
+
+        await role.load('permissions')
+        return role
+    }
+
+    async update({}){
+        const {permissions, ...data} = request.only([
+            "name",
+            "slug",
+            "description",
+            "permissions"
+        ])
+
+        const role = await Role.findOrFail(params.id)
+        role.merge(data)
+        await role.save()
+
+        if(permissions){
+            await role.permissions().sync(permissions)
+        }
+
+        await role.load('permissions')
+        return role
+    }
+
+    async destroy({params}){
+        const role = await Role.findOrFail(params.id)
+        return role.delete()
+    }
+}
+
+module.exports = RoleController
--- start/routes.js
+++ start/routes.js
@@ -13,4 +13,6 @@ Route.resource('users', 'UserController').apiOnly().middleware('auth.jwt')
 Route.resource('clients', 'ClientController').apiOnly().middleware('auth.jwt')
 Route.resource('exercises', 'ExerciseController').apiOnly().middleware('auth.jwt')
 Route.resource('training', 'TrainingController').apiOnly().middleware('auth.jwt')
+Route.resource('permissions', 'PermissionController').apiOnly().middleware('auth.jwt')
+Route.resource('roles', 'RoleController').apiOnly().middleware('auth.jwt')
 
